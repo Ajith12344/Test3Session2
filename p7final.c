1b9752c9bdd698daf4c5f98ca81ff58e640cda0c
Ajith111 <97220643+Ajith12344@users.noreply.github.com>
2022-03-03 08:49:36 +0000

    p 7

--- p7final.c
+++ p7final.c
@@ -1,1 +1,25 @@
-
+#include<stdio.h>
+struct _fraction
+{
+int num,den;
+};
+typedef _fraction Fraction;
+int gcd(int a, int b)
+{
+  int t;
+  while(b!=0)
+    {
+      t=b;
+      b=a%b;
+      a=t
+    }
+  
+}
+Fraction input_fraction();
+Fraction add_fractions(Fraction f1, Fraction f2)
+void output(Fraction f1, Fraction f2, Fraction f3, Fraction sum)
+int main()
+{
+  printf("%d\n",gcd(16,24));
+  return 0;
+  }
